4ce5a9276bd2ca0e30f53f40eec35d5ef0604cd4
debisic <55436065+debisic@users.noreply.github.com>
2024-09-16 00:12:47 +0200

    feature

--- docker/app/api.py
+++ docker/app/api.py
@@ -9,7 +9,7 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     """ Welcome page of the API """
-    return {'message': 'Welcome to the model API, to test the model go to <IP of VM/docs'>}
+    return {'message': 'Welcome to the model API, to test the model go to IP of VM/docs'}
 
 @app.post("/generate/")
 def generate_text(prompt: str):
